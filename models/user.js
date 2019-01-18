8b59683be67ff6a5fbe9a5b17576820b797ea20c
Pasoy <43441885+Pasoy@users.noreply.github.com>
2019-01-18 13:37:29 +0100

    changed keys

--- models/user.js
+++ models/user.js
@@ -4,18 +4,18 @@ const Joi = require('joi');
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        minlength: 2,
-        maxlength: 50
-    },
     email: {
         type: String,
         required: true,
         minlength: 5,
         maxlength: 255,
         unique: true
+    },
+    username: {
+        type: String,
+        required: true,
+        minlength: 2,
+        maxlength: 50
     },
     password: {
         type: String,
@@ -30,8 +30,8 @@ userSchema.methods.generateAuthToken = function() {
     const token = jwt.sign(
         {
             _id: this._id,
-            name: this.name,
             email: this.email,
+            username: this.username,
             isAdmin: this.isAdmin
         },
         config.get('jwtPrivateKey')
@@ -43,15 +43,15 @@ const User = mongoose.model('User', userSchema);
 
 function validateUser(user) {
     const schema = {
-        name: Joi.string()
-            .min(2)
-            .max(50)
-            .required(),
         email: Joi.string()
             .min(5)
             .max(255)
             .required()
             .email(),
+        username: Joi.string()
+            .min(2)
+            .max(50)
+            .required(),
         password: Joi.string()
             .min(5)
             .max(255)
